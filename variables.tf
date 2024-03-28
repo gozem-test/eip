0e6f4685728a4acd3c1e8338e0b3fe45f90ee2e2
Stéphane Noutsa <stephanenoutsak@gmail.com>
2024-03-28 16:09:48 +0100

    first commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SESSION_TOKEN" {
+  type    = string
+  default = null
+}
+
+variable "AWS_REGION" {
+  type = string
+}
